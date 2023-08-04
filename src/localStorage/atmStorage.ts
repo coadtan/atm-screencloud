a47594bb1101f8878ddacc401dc81450f4247c26
coadtan <mynamecoad@gmail.com>
2023-08-04 14:14:50 +0000

    init number of euro notes

--- src/localStorage/atmStorage.ts
+++ src/localStorage/atmStorage.ts
@@ -1,5 +1,8 @@
 const BALANCE_STORAGE_KEY = 'user-balance';
 const AUTH_STORAGE_KEY = 'isAuth';
+const NUMBER_OF_5_EURO_NOTE_STORAGE_KEY = 'number-of-5-euro-note';
+const NUMBER_OF_10_EURO_NOTE_STORAGE_KEY = 'number-of-10-euro-note';
+const NUMBER_OF_20_EURO_NOTE_STORAGE_KEY = 'number-of-20-euro-note';
 
 export const getBalance = (): number | null => {
   const balance = localStorage.getItem(BALANCE_STORAGE_KEY);
@@ -33,4 +36,10 @@ export const setAuth = (isAuthenticated: boolean): void => {
 
 export const removeAuthKey = () => {
   localStorage.removeItem(AUTH_STORAGE_KEY);
+};
+
+export const initialNote = () => {
+  localStorage.setItem(NUMBER_OF_5_EURO_NOTE_STORAGE_KEY, '4');
+  localStorage.setItem(NUMBER_OF_10_EURO_NOTE_STORAGE_KEY, '15');
+  localStorage.setItem(NUMBER_OF_20_EURO_NOTE_STORAGE_KEY, '7');
 };
