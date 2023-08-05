29f00f5b782689e7e170aedf93f246a085374f58
coadtan <mynamecoad@gmail.com>
2023-08-05 19:20:56 +0000

    add mocked store and mocked Reset hook to Atm page

--- src/pages/AtmPage.test.tsx
+++ src/pages/AtmPage.test.tsx
@@ -2,24 +2,50 @@ import * as routers from '@tanstack/router';
 import { render } from '../utils/tests/test-utils';
 import { AtmPage } from './AtmPage';
 import userEvent from '@testing-library/user-event';
+import { useBalanceStore } from '../stores/balanceStore';
+import { waitFor } from '@testing-library/dom';
+import { useResetScenario } from '../hooks/useResetScenario';
 
 vi.mock('../hooks/useCheckAuth');
+vi.mock('../hooks/useResetScenario');
 
 describe(AtmPage, () => {
-  test('should display Withdraw button', () => {
-    const view = render(<AtmPage />);
+  describe('display on screen', () => {
+    test('should display Withdraw button', () => {
+      const view = render(<AtmPage />);
 
-    const withdrawButton = view.getByRole('button', { name: /Withdraw/i });
+      const withdrawButton = view.getByRole('button', { name: /Withdraw/i });
 
-    expect(withdrawButton).toBeDefined();
-  });
+      expect(withdrawButton).toBeDefined();
+    });
 
-  test('should display History button', () => {
-    const view = render(<AtmPage />);
+    test('should display History button', () => {
+      const view = render(<AtmPage />);
 
-    const historyButton = view.getByRole('button', { name: /History/i });
+      const historyButton = view.getByRole('button', { name: /History/i });
 
-    expect(historyButton).toBeDefined();
+      expect(historyButton).toBeDefined();
+    });
+
+    test('should display current balance from store', () => {
+      useBalanceStore.setState({ currentBalance: 220 });
+
+      const view = render(<AtmPage />);
+
+      expect(view.getByText(/Your balance: £220/i)).toBeDefined();
+    });
+
+    test('should display Logging Out dialog when press "CANCEL"', async () => {
+      const user = userEvent.setup();
+
+      const view = render(<AtmPage />);
+
+      const cancelButton = view.getByRole('button', { name: /CANCEL/i });
+
+      await user.click(cancelButton);
+
+      expect(view.getByText(/Logging Out/i)).toBeDefined();
+    });
   });
 
   describe('navigation', () => {
@@ -51,6 +77,27 @@ describe(AtmPage, () => {
       await user.click(withdrawButton);
 
       expect(navigateSpy).toHaveBeenCalledWith({ to: '/history' });
+    });
+
+    test('should reset scenario when click "Logout" in Logging Out dialog', async () => {
+      const resetScenarioSpy = vi.fn();
+      vi.mocked(useResetScenario).mockImplementation(() => resetScenarioSpy);
+
+      const user = userEvent.setup();
+
+      const view = render(<AtmPage />);
+
+      const cancelButton = view.getByRole('button', { name: /CANCEL/i });
+
+      await user.click(cancelButton);
+
+      waitFor(() => expect(view.getByText(/Logging Out/i)).toBeDefined());
+
+      const logoutButton = view.getByRole('button', { name: /Logout/i });
+
+      await user.click(logoutButton);
+
+      expect(resetScenarioSpy).toHaveBeenCalled();
     });
   });
 });
--- src/setup-vitest.ts
+++ src/setup-vitest.ts
@@ -1,9 +1,15 @@
 import '@testing-library/jest-dom';
 
 vi.mock('zustand');
-vi.mock('@tanstack/router', () => ({
-  useNavigate: () => vi.fn(),
-}));
+vi.mock('@tanstack/router', async () => {
+  const actual = await vi.importActual('@tanstack/router');
+  return {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ...actual,
+    useNavigate: () => vi.fn(),
+  };
+});
 
 afterEach(() => {
   vi.restoreAllMocks();
